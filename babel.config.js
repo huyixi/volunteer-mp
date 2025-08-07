3e34208d10cab73d6e1e27d3114d846d22ce5a61
huyixi <hi@huyixi.com>
2025-08-07 11:48:56 +0800

    feat: add Taroify

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,38 @@
 // https://docs.taro.zone/docs/next/babel-config
 module.exports = {
   presets: [
-    ['taro', {
-      framework: 'react',
-      ts: true,
-      compiler: 'vite',
-    }]
-  ]
-}
+    [
+      "taro",
+      {
+        framework: "react",
+        ts: true,
+        compiler: "vite",
+      },
+    ],
+  ],
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "@taroify/core",
+        libraryDirectory: "",
+        style: true,
+      },
+      "@taroify/core",
+    ],
+    [
+      "import",
+      {
+        libraryName: "@taroify/icons",
+        libraryDirectory: "",
+        camel2DashComponentName: false,
+        style: () => "@taroify/icons/style",
+        customName: (name) =>
+          name === "Icon"
+            ? "@taroify/icons/van/VanIcon"
+            : `@taroify/icons/${name}`,
+      },
+      "@taroify/icons",
+    ],
+  ],
+};
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,51 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text, Image } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+// 插件会自动将代码转化为方式二中的按需引入形式
+import { Button } from "@taroify/core";
+import { Swiper } from "@taroify/core";
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
 
   return (
-    <View className='index'>
+    <View className="index">
+      <Swiper className="image-swiper" lazyRender autoplay={4000}>
+        <Swiper.Indicator />
+        <Swiper.Item>
+          <Image
+            className="image"
+            src="https://img01.yzcdn.cn/vant/apple-1.jpg"
+          />
+        </Swiper.Item>
+        <Swiper.Item>
+          <Image
+            className="image"
+            src="https://img01.yzcdn.cn/vant/apple-2.jpg"
+          />
+        </Swiper.Item>
+        <Swiper.Item>
+          <Image
+            className="image"
+            src="https://img01.yzcdn.cn/vant/apple-3.jpg"
+          />
+        </Swiper.Item>
+        <Swiper.Item>
+          <Image
+            className="image"
+            src="https://img01.yzcdn.cn/vant/apple-4.jpg"
+          />
+        </Swiper.Item>
+      </Swiper>
       <Text>Hello world!</Text>
+      <Button color="primary">主要按钮</Button>
+      <Button color="info">信息按钮</Button>
+      <Button color="success">成功按钮</Button>
+      <Button color="warning">警告按钮</Button>
+      <Button color="danger">危险按钮</Button>
+      <Button color="default">默认按钮</Button>
     </View>
-  )
+  );
 }
